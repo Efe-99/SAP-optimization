072bdfbac88cd0a1e97046fcafda69d7d78fe444
Efe-99 <wealthmay12@gmail.com>
2025-11-19 23:23:15 -0500

    Create App.js

---
+++ sap-demo/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NeptuneSAPDemo from './components/NeptuneSAPDemo';
+
+function App() {
+  return (
+    <div className="App">
+      <NeptuneSAPDemo />
+    </div>
+  );
+}
+
+export default App;
